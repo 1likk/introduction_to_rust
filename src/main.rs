f7df6f4ab53f8ab64ae044ff3a795c0d6e047a24
Birlik Zhumadil <zhbirlik.2006@gmail.com>
2026-03-13 11:41:27 +0500

    first look

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+fn main(){
+    let tup = (500, 6.1, 1);
+    let (x, y, z) = tup;
+
+    println!("{x}");
+    condition();
+    loops();
+    while_loop();
+}
+
+fn condition(){
+    let number = 3;
+
+    if number < 5 {
+        println!("condition was true");
+    }
+    else {
+        println!("condition was false");
+    }
+}
+
+fn loops(){
+    let mut n = 0;
+    loop {
+        println!("again again");
+        n = n + 1;
+        if n == 10 {
+            break;
+        }
+    }
+}
+
+fn while_loop(){
+    let mut i = 1;
+    let mut j = 1;
+    while i < 10 {
+        while j < 10 {
+            print!("{}\t", i * j);
+            j = j + 1;
+        }
+        println!();
+        i = i + 1;
+        j = 1;
+    }
+}
+
