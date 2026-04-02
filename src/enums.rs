30cf4f233ea28c1566ff4fbe4d9ddc805bd4eee7
Birlik Zhumadil <zhbirlik.2006@gmail.com>
2026-04-02 11:26:32 +0500

    enum conception

---
+++ src/enums.rs
@@ -0,0 +1,25 @@
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message { 
+    Quit, 
+    Move { x: i32, y: i32},
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+fn main(){
+    let home = IpAddr::V4((127, 0, 0, 1));
+    let lookback = IpAddr::V6(String::from("::1"));
+}
+
+fn route(ip_kind: IpAddr) {
+
+}
+
+enum Option<T> {
+    None,
+    Some(T),
+}
