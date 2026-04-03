43dd9f1a8eaf991abd9b0ea6325c23dd63144b36
Birlik Zhumadil <zhbirlik.2006@gmail.com>
2026-04-03 12:15:56 +0500

    coins rs for flow control

---
+++ src/coins.rs
@@ -0,0 +1,56 @@
+enum UsState {
+    Alabama, 
+    Alaska,
+    Arizona,
+    Arkansas,
+    California,
+    Colorado,
+    Connecticut,
+    Delaware,
+    Florida,
+    Georgia,
+    Hawaii,
+    Idaho,
+    Illinois,
+    Indiana,
+    Iowa,
+    Kansas,
+    Kentucky,
+    Louisiana,
+    Maine,
+    Maryland,
+    Massachusetts,
+    Michigan,
+    Minnesota,
+    Mississippi,
+    Missouri,
+    Montana,
+    Nebraska,
+}
+
+enum Coin {
+    Penny, 
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {state:?}!");
+            25
+        }
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
+}
+
--- src/practice.rs
+++ src/practice.rs
@@ -1,7 +1,13 @@
+enum Category {
+    fproduct(f64),
+    dproduct(String),
+}
+
 struct Product {
     name: String,
     price: f64,
     stock: u32,
+    category: Category,
 }
 
 impl Product {
@@ -18,6 +24,32 @@ fn main() {
         stock: 100,
     };
 
+    let result = find_product(2);
+
+    match result {
+        Some(name) => println!("We have that: {}.", name),
+        None => println!("We haven't that sorry!"),
+    }
+
     println!("A new cost of chocalate with discount: {}.", product1.update_cost())
 }
 
+fn print_info(category: Category) {
+    match category {
+        Category::fproduct(weight) => {
+            println!("This is a physical product, weight {}.", weight);
+        },
+        Category::dproduct(link) => {
+            println!("This is a digital product,link {}.", link);
+        },
+    }
+}
+
+
+fn find_product(id: u32) -> Option<String> {
+    if id == 1 {
+        Some(String::from("chocolate"))
+    } else {
+        None
+    }
+}
