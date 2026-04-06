6732beb361acc2dcaf98de9529471c910dc03c3a
Birlik Zhumadil <zhbirlik.2006@gmail.com>
2026-04-06 16:53:43 +0500

    errors

--- src/errors.rs
+++ src/errors.rs
@@ -1,5 +1,19 @@
 fn main() {
     //panic!("crash and burn");
+    let greeting_life_result = File::open("hello.txt");
+
+    let greeting_life = match greeting_life_result {
+        Ok(file) => file, 
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {e:?}"),
+            },
+            _ => {
+                panic!("Problem opening the file: {error:?}");
+            }
+        },
+    };
 
     example();
 }
@@ -9,3 +23,4 @@ fn example() {
     
     v[99];
 }
+
